f4f449e4157c94fef2d809822eb72104a7a051ad
Richard J. de Borja <richard.deborja@uhnresearch.ca>
2017-08-31 17:36:56 -0400

    new setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name="star",
+    version="0.1",
+    license="MIT",
+    author="Richard J. de Borja",
+    author_email="richard.deborja@uhnresearch.ca",
+    description="A wrapper for the STAR and STAR-Fusion programs.",
+    py_modules=['star']
+    )
