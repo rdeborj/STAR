ce3b8a503d415e7830a4a28724504c03e79768c3
Richard J. de Borja <richard.deborja@uhnresearch.ca>
2017-08-31 17:22:04 -0400

    removed config files, added test for fusions, code cleanup

---
+++ tests/test_star_fusion.py
@@ -0,0 +1,17 @@
+import unittest
+from Star import get_fusions
+
+class TestFusion(unittest.TestCase):
+    def test_fusion_command(self):
+        fusions = get_fusions(
+            junction='STAR/LTS-035_T_RNA_GCCAAT_L007Chimeric.out.junction',
+            prefix='STAR_fusion/LTS-035_T_RNA_GCCAAT_L007',
+            out_sam='STAR/LTS-035_T_RNA_GCCAAT_L007Chimeric.out.sam',
+            gtf='/cluster/projects/carlsgroup/workinprogress/abdel/20170418_INSPIRE_RNA/gencode.v26.annotation.gtf')
+        expected_command = " ".join([
+            'STAR-Fusion',
+            '--chimeric_junction STAR/LTS-035_T_RNA_GCCAAT_L007Chimeric.out.junction',
+            '--chimeric_out_sam STAR/LTS-035_T_RNA_GCCAAT_L007Chimeric.out.sam',
+            '--ref_GTF /cluster/projects/carlsgroup/workinprogress/abdel/20170418_INSPIRE_RNA/gencode.v26.annotation.gtf',
+            '--out_prefix STAR_fusion/LTS-035_T_RNA_GCCAAT_L007'])
+        self.assertEqual(fusions['command'], expected_command)
